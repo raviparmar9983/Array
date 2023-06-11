b0704fa6659e1bf996679b80b741eefbe5831009
Ravi Parmar <parmarravi1162@gmail.com>
2023-06-11 19:46:02 +0530

    change

--- Pair.java
+++ Pair.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Pair {
     public static int sum(int arr[],int n,int sum){
-        
+        int count =0;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(arr[i]+arr[j]==sum){
-                    return 1;
+                    count++;
                 }
             }
         }
         
-        return 0;
+        return count;
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
